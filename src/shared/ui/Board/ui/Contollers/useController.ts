d4998702def776d1b5ff013d81e6dac86508518c
kdastation <monnicmambo2281337@gmail.com>
2024-12-09 09:36:13 +0300

    refactor: refactoring controller

--- src/shared/ui/Board/ui/Contollers/useController.ts
+++ src/shared/ui/Board/ui/Contollers/useController.ts
@@ -1,27 +1,112 @@
+import { useEventsPublic } from '../Events/Public'
+import { createArrow, createLine, createRectangle } from '../Shapes/creators'
 import { type Controller } from './Controller'
-import { useArrowController, type ArrowControllerArgs } from './useArrowController'
-import { useMiltyLineController, type MultiLineControllerArgs } from './useMultiLineController'
-import { useSelectController, type SelectControllerArgs } from './useSelectController'
-import { useTargetController, type TargetControllerArgs } from './useTargetController'
+import { useArrowController } from './useArrowController'
+import { useMiltyLineController } from './useMultiLineController'
+import { useSelectController } from './useSelectController'
+import { useTargetController } from './useTargetController'
 
 export type StateController = 'idle' | 'add' | 'drag' | 'edit-text' | 'arrow' | 'multi-line'
 
 export const useController = (
   state: StateController,
   controllers: {
-    select: SelectControllerArgs
-    target: TargetControllerArgs
-    arrow: ArrowControllerArgs
-    multiLine: MultiLineControllerArgs
+    select: {
+      selected: string[]
+    }
   }
 ): Controller => {
-  const selectController = useSelectController(controllers.select)
+  const { onEvent } = useEventsPublic()
 
-  const targetController = useTargetController(controllers.target)
+  const selectController = useSelectController({
+    onSelect: (ids) => {
+      onEvent?.({
+        type: 'select',
+        ids,
+      })
+    },
+    selected: controllers.select.selected,
+  })
 
-  const arrowController = useArrowController(controllers.arrow)
+  const targetController = useTargetController({
+    onTarget: ({ x, y }) => {
+      const newRectangle = createRectangle({
+        scale: {
+          x: 1,
+          y: 1,
+        },
+        width: 400,
+        height: 400,
+        x,
+        y,
+        fill: '#4387b1',
+        rotation: 0,
+      })
 
-  const multiLineController = useMiltyLineController(controllers.multiLine)
+      onEvent?.({
+        type: 'add-shape',
+        shape: newRectangle,
+      })
+
+      onEvent?.({
+        type: 'select',
+        ids: [newRectangle.id],
+      })
+
+      onEvent?.({
+        type: 'change-tool',
+        tool: 'idle',
+      })
+    },
+  })
+
+  const arrowController = useArrowController({
+    onAdd: (points) => {
+      const newArrow = createArrow({
+        points: [points.start.x, points.start.y, points.end.x, points.end.y],
+        stroke: 'red',
+      })
+
+      onEvent?.({
+        type: 'add-shape',
+        shape: newArrow,
+      })
+
+      onEvent?.({
+        type: 'select',
+        ids: [newArrow.id],
+      })
+
+      onEvent?.({
+        type: 'change-tool',
+        tool: 'idle',
+      })
+    },
+  })
+
+  const multiLineController = useMiltyLineController({
+    onAdd: (points) => {
+      const newLine = createLine({
+        points,
+        stroke: 'red',
+      })
+
+      onEvent?.({
+        type: 'add-shape',
+        shape: newLine,
+      })
+
+      onEvent?.({
+        type: 'select',
+        ids: [newLine.id],
+      })
+
+      onEvent?.({
+        type: 'change-tool',
+        tool: 'idle',
+      })
+    },
+  })
 
   if (state === 'idle') {
     return selectController
--- src/shared/ui/Board/ui/Root.tsx
+++ src/shared/ui/Board/ui/Root.tsx
@@ -6,10 +6,9 @@ import useResizeObserver from 'use-resize-observer'
 import { useController, type StateController } from './Contollers/useController'
 import { useZoomController } from './Contollers/useZoomController'
 import { DragDrop } from './DragDrop/DragDrop'
-import { EventsPublicProvider, useEventsPublic, type OnEvent } from './Events/Public'
+import { EventsPublicProvider, type OnEvent } from './Events/Public'
 import { SceneProvider, useScene } from './Scene/SceneProvider'
 import { type Shape } from './Shape'
-import { createArrow, createLine, createRectangle } from './Shapes/creators'
 import { FactoryShapes } from './Shapes/FactoryShapes'
 import { StageStoreProvider, useStageStore } from './Store/StageStore'
 import { Transform } from './Transform'
@@ -31,94 +30,9 @@ const Board = ({
 
   const { setStage, ...stage } = stageStore((state) => state)
 
-  const { onEvent } = useEventsPublic()
-
   const { elements, stageProps } = useController(tool, {
     select: {
-      onSelect: (ids) => {
-        onEvent?.({
-          type: 'select',
-          ids,
-        })
-      },
       selected,
-    },
-    target: {
-      onTarget: ({ x, y }) => {
-        const newRectangle = createRectangle({
-          scale: {
-            x: 1,
-            y: 1,
-          },
-          width: 400,
-          height: 400,
-          x,
-          y,
-          fill: '#4387b1',
-          rotation: 0,
-        })
-
-        onEvent?.({
-          type: 'add-shape',
-          shape: newRectangle,
-        })
-
-        onEvent?.({
-          type: 'select',
-          ids: [newRectangle.id],
-        })
-
-        onEvent?.({
-          type: 'change-tool',
-          tool: 'idle',
-        })
-      },
-    },
-    arrow: {
-      onAdd: (points) => {
-        const newArrow = createArrow({
-          points: [points.start.x, points.start.y, points.end.x, points.end.y],
-          stroke: 'red',
-        })
-
-        onEvent?.({
-          type: 'add-shape',
-          shape: newArrow,
-        })
-
-        onEvent?.({
-          type: 'select',
-          ids: [newArrow.id],
-        })
-
-        onEvent?.({
-          type: 'change-tool',
-          tool: 'idle',
-        })
-      },
-    },
-    multiLine: {
-      onAdd: (points) => {
-        const newLine = createLine({
-          points,
-          stroke: 'red',
-        })
-
-        onEvent?.({
-          type: 'add-shape',
-          shape: newLine,
-        })
-
-        onEvent?.({
-          type: 'select',
-          ids: [newLine.id],
-        })
-
-        onEvent?.({
-          type: 'change-tool',
-          tool: 'idle',
-        })
-      },
     },
   })
 
@@ -130,7 +44,6 @@ const Board = ({
     <DragDrop.Container ref={ref}>
       <Stage
         style={{
-          border: '1px solid black',
           width: '100%',
         }}
         ref={stageRef}
